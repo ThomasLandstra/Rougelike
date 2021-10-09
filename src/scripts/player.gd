8f6c8c90a031d71b40e7603be578c96989a2da2c
Thomas Landstra <thomaslandstra@gmail.com>
2021-10-09 16:45:55 +1100

    Linked animation to movement

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -3,23 +3,33 @@ extends KinematicBody2D
 
 # Variables
 var movement : Vector2 = Vector2.ZERO
+var direction : String = "South"
+var last_direction : String = ""
 const MAX_SPEED : int = 50
 
-# Called at start
+
+
 func _ready():
+	# Set animations to idle
 	$AnimationPlayer.play("Idle_South")
-	
+
+
+
 # Called once per frame
-func _physics_process(delta):
-	var axis = get_input_axis() # Get direction
-	
-	if axis == Vector2.ZERO: # Stop moving
-		movement = Vector2.ZERO
-	else: # Start/Continue moving
-		apply_movement(axis)
-	
+func _physics_process(_delta):
+	# Get direction and movement axis
+	movement = get_input_axis()
+	get_direction(movement)
+
+	# Get movement
+	if not movement == Vector2.ZERO: # If moving
+		# Set speed
+		movement.x *= MAX_SPEED
+		movement.y *= MAX_SPEED
+
 	# Move
 	move_and_slide(movement)
+	apply_animation()
 
 
 func get_input_axis(): 
@@ -27,7 +37,69 @@ func get_input_axis():
 	axis.x = int(Input.is_key_pressed(KEY_D) or Input.is_action_pressed("ui_right")) - int(Input.is_key_pressed(KEY_A)  or Input.is_action_pressed("ui_left"))
 	axis.y = int(Input.is_key_pressed(KEY_S) or Input.is_action_pressed("ui_down")) - int(Input.is_key_pressed(KEY_W)  or Input.is_action_pressed("ui_up"))
 	return axis.normalized()
-	
-func apply_movement(move_axis):
-	movement.x = move_axis.x * MAX_SPEED
-	movement.y = move_axis.y * MAX_SPEED
+
+func get_direction(axis : Vector2):
+	# Moving North/South
+	if axis.x == 0:
+		if axis.y == -1:
+			direction = "North"
+		elif axis.y == 1:
+			direction = "South"
+
+		# Clean last_direction
+		last_direction = ""
+
+	# Moving East/West
+	elif axis.y == 0:
+		if axis.x == -1:
+			direction = "West"
+		elif axis.x == 1:
+			direction = "East"
+		last_direction = ""
+
+	# Moving Diagonal
+	else:
+		# Set last_direction
+		if last_direction == "":
+			last_direction = direction
+
+		# Moving Right
+		if axis.x > 0:
+
+			# Moving Down
+			if axis.y > 0:
+				if last_direction == "East":
+					direction = "South"
+				else:
+					direction = "East"
+
+			# Moving up
+			else:
+				if last_direction == "East":
+					direction = "North"
+				else:
+					direction = "East"
+
+		# Moving Left
+		else:
+
+			# Moving Up
+			if axis.y > 0:
+				if last_direction == "West":
+					direction = "South"
+				else:
+					direction = "West"
+
+			# Moving Down
+			else:
+				if last_direction == "West":
+					direction = "North"
+				else:
+					direction = "West"
+
+
+func apply_animation():
+	if movement == Vector2.ZERO:
+		$AnimationPlayer.play("Idle_"+direction)
+	else:
+		$AnimationPlayer.play("Run_"+direction)
