2c8345e38c588ba2c937943f1f7ad2ac7d508365
The Undercover CEO <55495874+TheUndercoverCEO@users.noreply.github.com>
2021-11-16 18:04:42 +1100

    Fix pathfinding

--- src/scripts/zombie.gd
+++ src/scripts/zombie.gd
@@ -1,55 +1,64 @@
 extends KinematicBody2D
 
-
-# Variables
-var movement: Vector2 = Vector2.ZERO
-var direction: String = "South"
-var last_direction: String = ""
 const MAX_SPEED: int = 48
+var velocity: Vector2 = Vector2.ZERO
 
-var path: Array = []
-var level_navigation: Navigation2D
+var path: Array = []	# Contains destination positions
+var levelNavigation: Navigation2D = null
 var player = null
-var player_spotted: bool = false
-var enemies: Array
+var last_pos: Vector2
+var spotted: bool = false
 
 onready var line2d = $Line2D
-onready var sight = $sight
+onready var los = $sight
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	yield(get_tree(), "idle_frame")
 	var tree = get_tree()
-	level_navigation = tree.get_nodes_in_group("level_navigation")[0]
-	player = tree.get_nodes_in_group("player")[0]
-	enemies = tree.get_nodes_in_group("enemies")
+	if tree.has_group("level_navigation"):
+		levelNavigation = tree.get_nodes_in_group("level_navigation")[0]
+	if tree.has_group("player"):
+		player = tree.get_nodes_in_group("player")[0]
 
-# Called every frame
-func _physics_process(_delta):
+func _physics_process(delta):
 	line2d.global_position = Vector2.ZERO
-	if player and level_navigation:
-		check_player_detection()
-		if player_spotted:
-			navigate()
+	if player:
+		spotted = check_player_in_detection()
+		if spotted:
+			generate_path()
+		navigate(delta)
 	move()
 
-func check_player_detection():
-	sight.look_at(player.global_position)
-	var collider = sight.get_collider()
-	if collider.is_in_group("player"):
-		player_spotted = true
+func navigate(delta):	# Define the next position to go to
+	if path.size() > 0 and spotted:
+		velocity = global_position.direction_to(path[1]) * MAX_SPEED * (delta + 1)
+		
+		# If reached the destination, remove this point from path array
+		if global_position == path[0]:
+			path.pop_front()
+	elif path.size() > 1:
+		velocity = global_position.direction_to(path[1]) * MAX_SPEED * (delta + 1)
+		
+		# If reached the destination, remove this point from path array
+		if global_position.distance_to(path[0]) < 13:
+			path.pop_front()
+	else: velocity = Vector2.ZERO
+
+func generate_path():	# It's obvious
+	if levelNavigation != null and player != null:
+		path = levelNavigation.get_simple_path(global_position, player.global_position, false)
+		line2d.points = path
+
+
+func check_player_in_detection() -> bool:
+	los.look_at(player.global_position)
+	var collider = los.get_collider()
+	if collider and collider.is_in_group("player"):
+		return true
 	else:
-		player_spotted = false
-
-func navigate():
-	movement = global_position.direction_to(player.global_position) * MAX_SPEED
-	var collider
-	for x in enemies:
-		sight.look_at(x)
-		collider = sight.get_collider()
-		if collider.is_in_group("enemies"):
-			pass
-
+		return false
+	return false
 
 func move():
-	move_and_slide(movement)
+	velocity = move_and_slide(velocity)
