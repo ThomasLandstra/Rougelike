28dd15a10e09e140c3dafb29c6624d246ffa6a18
Thomas Landstra <thomaslandstra@gmail.com>
2021-11-18 16:35:36 +1100

    Refactor code and increase performance

--- src/scripts/zombie.gd
+++ src/scripts/zombie.gd
@@ -1,69 +1,70 @@
 extends KinematicBody2D
 
+# Movement
 const MAX_SPEED: int = 48
 const LOS_FIX: Vector2 = Vector2(8,8)
-var velocity: Vector2 = Vector2.ZERO
+var movement: Vector2 = Vector2.ZERO
 
-var path: Array = []	# Contains destination positions
+# Navigation
+var path: Array = []
 var levelNavigation: Navigation2D = null
 var player = null
-var last_pos: Vector2
 var spotted: bool = false
-var last_direction: String = "East"
 
-onready var line2d = $Line2D
+# Animation
+var direction: String = "East"
+
+# Sight and Path
+onready var line = $Line2D
 onready var los = $sight
 
 
 func _ready():
+	# Get tree
 	yield(get_tree(), "idle_frame")
 	var tree = get_tree()
-	if tree.has_group("level_navigation"):
-		levelNavigation = tree.get_nodes_in_group("level_navigation")[0]
-	if tree.has_group("player"):
-		player = tree.get_nodes_in_group("player")[0]
+	
+	# Get variables
+	levelNavigation = tree.get_nodes_in_group("level_navigation")[0]
+	player = tree.get_nodes_in_group("player")[0]
 
 func _physics_process(delta):
-	line2d.global_position = Vector2.ZERO
-	spotted = check_player_in_detection()
-	
-	if player:
+	if levelNavigation != null and player != null:
+		# Set raycast position
+		line.global_position = Vector2.ZERO
+		spotted = check_player_in_detection()
+		
+		# If spotted, generate path
 		if spotted:
-			generate_path()
-
-		navigate(delta)
-		if velocity != Vector2.ZERO:
-			get_direction(velocity)
-			$AnimationPlayer.play("Run_"+last_direction)
-			velocity = move_and_slide(velocity.clamped(MAX_SPEED))
+			path = levelNavigation.get_simple_path(global_position, player.global_position+LOS_FIX, false)
+		elif path.size() > 0:
+			path = levelNavigation.get_simple_path(global_position, path[-1], false)
+		
+		navigate(delta) # Generate movement
+		
+		# Move and animate
+		if movement != Vector2.ZERO:
+			get_direction(movement)
+			$AnimationPlayer.play("Run_"+direction)
+			movement = move_and_slide(movement.clamped(MAX_SPEED))
 		else:
-			$AnimationPlayer.play("Idle_"+last_direction)
+			$AnimationPlayer.play("Idle_"+direction)
 
-func navigate(delta):	# Define the next position to go to
-	if path.size() > 0 and spotted:
-		velocity = global_position.direction_to(path[1]) * MAX_SPEED * (delta + 1)
+# Define the next position to go to
+func navigate(delta):
+	if path.size() > 0:
+		movement = global_position.direction_to(path[1]) * MAX_SPEED * (delta + 1)
 
 		# If reached the destination, remove this point from path array
 		if global_position == path[0]:
 			path.pop_front()
-	elif path.size() > 1:
-		velocity = global_position.direction_to(path[1]) * MAX_SPEED * (delta + 1)
-
-		# If reached the destination, remove this point from path array
-		if global_position.distance_to(path[0]) < 13:
-			path.pop_front()
-	else: velocity = Vector2.ZERO
-
-func generate_path():	# It's obvious
-	if levelNavigation != null and player != null:
-		path = levelNavigation.get_simple_path(global_position, player.global_position, false)
-		line2d.points = path
-
+			line.points = path
+		if path.size() == 2: path = []
+	else: movement = Vector2.ZERO
 
 func check_player_in_detection() -> bool:
 	if player != null:
 		los.look_at(player.global_position + LOS_FIX)
-		los.visible = true
 		var collider = los.get_collider()
 		if collider and collider.is_in_group("player"):
 			return true
@@ -81,13 +82,6 @@ func get_direction(axis : Vector2):
 		negative_y = true
 
 	if axis.x > axis.y:
-		if negative_x:
-			last_direction = "West"
-		else:
-			last_direction = "East"
-	if axis.x < axis.y:
-		if negative_y:
-			last_direction = "North"
-		else:
-			last_direction = "South"
-
+		direction = "West" if negative_x else "East"
+	elif axis.x < axis.y:
+		direction = "North" if negative_y else "South"
